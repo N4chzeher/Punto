b0680bf2440542cfd5ae46e681caf079c3cf2d6c
n4chzeher <diegorrojas@hotmail.com>
2017-03-03 23:02:28 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * Created by NACHZEHER on 03/03/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("PRIMER PUNTO");
+
+        System.out.println("Coordenada x:");
+        double x =sc.nextDouble();
+        System.out.println("Coordenada y:");
+        double y =sc.nextDouble();
+
+        Punto punto1 =new Punto(x,y);
+
+        System.out.println("Desplazar x:");
+
+        double dx=sc.nextDouble();
+
+        punto1.desplaza(dx);
+
+        System.out.println("Desplazar x & y:");
+
+        double dy=sc.nextDouble();
+
+        punto1.desplaza(dx,dy);
+
+        System.out.println("NUEVO PUNTO");
+
+        System.out.println("Coordenada x:");
+        double x2 =sc.nextDouble();
+        System.out.println("Coordenada y:");
+        double y2 =sc.nextDouble();
+
+        Punto otro =new Punto(x2,y2);
+
+
+        System.out.println("La distancia entre puntos es "+punto1.distanciaEuclidea(otro));
+
+        punto1.muestra();
+
+
+
+    }
+}
---
+++ src/Punto.java
@@ -0,0 +1,39 @@
+/**
+ * Created by NACHZEHER on 03/03/2017.
+ */
+public class Punto {
+
+    private double x;
+    private double y;
+
+    public Punto (double x, double y){
+
+        this.x=x;
+        this.y=y;
+    }
+
+    public  void desplaza(double dx){
+
+        x += dx;
+    }
+
+    public  void desplaza(double dx, double dy){
+
+        x += dx;
+        y += dy;
+    }
+
+    public double distanciaEuclidea(Punto otro){
+
+        double d = Math.sqrt( Math.pow(otro.x-x, 2) + Math.pow(otro.y-y, 2) );
+
+        return d;
+    }
+
+    public void muestra(){
+
+        System.out.println("Coordenada x = "+x+"\nCoordenada y = "+y);
+    }
+
+
+}
